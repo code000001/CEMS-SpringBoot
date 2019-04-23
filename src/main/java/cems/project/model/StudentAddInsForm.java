a889b14ff9c2f23016b729d4375df531d97bc367
Phakhanan Thongmee <arunsittiwakrai@hotmail.com>
2019-04-24 01:34:37 +0700

    student quali status update

--- src/main/java/cems/project/model/StudentAddInsForm.java
+++ src/main/java/cems/project/model/StudentAddInsForm.java
@@ -22,6 +22,15 @@ public class StudentAddInsForm {
 		
 	}
 	
+	@Column(name = "std_status_id")
+	private Integer stdStatusId;
+	public Integer getStdStatusId() {
+		return stdStatusId;
+	}
+	public void setStdStatusId(Integer stdStatusId) {
+		this.stdStatusId = stdStatusId;
+	}
+	
 	@Column(name = "std_id")
 	private Integer stdId;
 	public Integer getStdId() {
@@ -29,6 +38,32 @@ public class StudentAddInsForm {
 	}
 	public void setStdId(Integer stdId) {
 		this.stdId = stdId;
+	}
+	
+	@Column(name = "std_test_score")
+	private String stdTestScore;
+	public String getStdTestScore() {
+		return stdTestScore;
+	}
+	public void setStdTestScore(String stdTestScore) {
+		this.stdTestScore = stdTestScore;
+	}
+	
+	@Column(name = "std_hr_prepare")
+	private Integer stdHrPrepare;
+	public Integer getStdHrPrepare() {
+		return stdHrPrepare;
+	}
+	public void setStdHrPrepare(Integer stdHrPrepare) {
+		this.stdHrPrepare = stdHrPrepare;
+	}
+	@Column(name = "std_hr_conference")
+	private Integer stdHrConference;
+	public Integer getStdHrConference() {
+		return stdHrConference;
+	}
+	public void setStdHrConference(Integer stdHrConference) {
+		this.stdHrConference = stdHrConference;
 	}
 	
 	@Column(name = "std_th_prefix")
